6423ae9a6abcceb03d517871cca2f40f886b1287
Liam Monahan <liam@monahan.io>
2015-04-11 14:12:35 -0400

    Added functions to load and persist address info.

--- bin/dataparser.py
+++ bin/dataparser.py
@@ -4,35 +4,46 @@ import creds
 import json
 import requests
 import sys
-from datetime import date, datetime
 
 save_path = "../data/%s"
 pristine_data_path = save_path % "stalled-construction-sites-pristine.json"
 trimmed_data_path = save_path % "stalled-construction-after-2013.json"
+addresses_data_path = save_path % "addresses.json"
 
 GOOGLE_API_KEY = creds.GOOGLE_API_KEY
 
+ADDRESSES = {}
+
+
+def load_addresses():
+    global ADDRESSES
+    ADDRESSES = read_json(addresses_data_path)['addresses']
+
+
+def save_addresses():
+    write_json(addresses_data_path, {"addresses": ADDRESSES})
+
 
 def main():
-    # retrieve all entries from pristine data where
-    # complaint date falls after Jan 1st 2013
-    # filter_date = date(2013, 1, 1)
-    # new_data_path = trimmed_data_path
-    # trim_data(new_data_path)
+    new_data_path = trimmed_data_path
+    trim_data(new_data_path)
+
+    load_addresses('../data/addresses.json')
+
     output = {}
     output['objects'] = []
     output_json = output['objects']
-    
+
     existing_addresses = {}
-    
+
     input_json = read_json(trimmed_data_path)
     input_objects = input_json['objects']
-    for complaint_entry in input_objects[:5]:
+    for complaint_entry in input_objects[:50]:
         address = get_address_from_entry(complaint_entry)
         complaint = get_complaint_dict(complaint_entry)
         if address in existing_addresses:
-             existing_address = existing_addresses[address]
-             existing_address['complaints'].append(complaint)
+            existing_address = existing_addresses[address]
+            existing_address['complaints'].append(complaint)
         else:
             new_address = get_google_maps_dict_from_entry(complaint_entry)
             if new_address is None:
@@ -41,8 +52,9 @@ def main():
             new_address['complaints'] = [complaint]
             existing_addresses[address] = new_address
     print json.dumps(existing_addresses,
-            indent=4, separators=(',', ':'))
+                     indent=4, separators=(',', ':'))
 
+    save_addresses('../data/addresses.json')
 
 
 def get_complaint_dict(complaint_entry):
@@ -68,8 +80,16 @@ def get_address_from_entry(entry):
     return address
 
 
+def persist_address(address_string, coordinates):
+    '''
+    Write the coordnates hash to a json file if not already cached
+    '''
+    if address_string not in ADDRESSES:
+        ADDRESSES[address_string] = coordinates
+
+
 def get_google_maps_result(location_string):
-    print "getting google maps result for location string '%s'" % location_string
+    print "getting google maps result for '%s'" % location_string
 
     resp = requests.get(
         'https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s' %
@@ -87,7 +107,7 @@ def trim_data(save_path):
     """Using the given json data, filter out the stalled construction
     entries that fall before or after a certain date and return the
     data as a json file. """
-    
+
     complaint_numbers = {}
     output_json = {}
     output_json['objects'] = []
@@ -96,11 +116,7 @@ def trim_data(save_path):
     with open(pristine_data_path) as data_file:
         pristine_data = json.load(data_file)
         data_entries = pristine_data['data']
-        complaint_date_idx = 14
-        json_date_format = "%Y-%m-%dT%H:%M:%S"
         for entry in data_entries:
-            raw_date = entry[complaint_date_idx]
-            datetime_obj = datetime.strptime(raw_date, json_date_format)
             complaint_number = entry[13]
             if complaint_number in complaint_numbers:
                 continue
@@ -112,10 +128,14 @@ def trim_data(save_path):
 
 
 def read_json(file_path):
-    with open(file_path) as data:
+    with open(file_path, 'r') as data:
         json_data = json.load(data)
         return json_data
 
+
+def write_json(file_path, json_to_write):
+    with open(file_path, 'w+') as f:
+        json.dump(json_to_write, f, indent=4)
 
 if "__main__" == __name__:
     main()
